c97fa67ec5a707879f9ab7207ddf40f69540b0ad
Carlos Vásquez Polanco <carlosslim@gmail.com>
2013-10-22 01:02:32 -0700

    Refactorización de clase

--- src/main/java/org/devdom/service/SkillsetResource.java
+++ src/main/java/org/devdom/service/SkillsetResource.java
@@ -7,13 +7,13 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
+import javax.ws.rs.core.UriInfo;
 import org.devdom.model.dao.CategoryDao;
 import org.devdom.model.dao.SkillsDao;
-import org.devdom.model.dto.Pagination;
 import org.devdom.model.dto.Skills;
-import org.devdom.model.dto.Skillset;
+import org.devdom.model.dto.MasterSkillset;
 
 /**
  *
@@ -22,70 +22,35 @@ import org.devdom.model.dto.Skillset;
 
 @Path("/skill")
 public class SkillsetResource {
-    
-    Skillset skillset = new Skillset();
+
     SkillsDao skillsDao = new SkillsDao();
-    CategoryDao category = new CategoryDao();
-    private final int ROWS_PER_PAGE = 10;
-    private int from = 0;
-    private int to = 0;
-    private int rowCount = 0;
-    private int currentPage = 1;
+    CategoryDao categoryDao = new CategoryDao();
     
     @GET
     @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
     @Path("by/category/id/{id}")
-    public Skillset findSkillsByCategoryId(@PathParam("id") int id,
-                                           @HeaderParam("Accept") String acceptHeader
+    public MasterSkillset findSkillsByCategoryId(@PathParam("id") int id,
+                                                 @HeaderParam("Accept") String acceptHeader,
+                                                 @Context UriInfo uri
                                             ){
 
-        currentPage = 1; //Siempre se llama la primera página desde este método
+        String path = categoryDao.getRealPath(uri.getAbsolutePath().toString());
 
-        from = (currentPage-1)*ROWS_PER_PAGE;
-        to = (from+ROWS_PER_PAGE);
-        
-        List<Skills> skills = skillsDao.findSkillsByCategoryId(id);
-        rowCount = skills.size();
-        
-        to = (to>rowCount)?rowCount:to;
-        
-        skills = skills.subList(from,to); 
-        Pagination pagination = new Pagination(currentPage,ROWS_PER_PAGE,id,rowCount,acceptHeader);
-        
-        skillset.setPagination(pagination);
-        skillset.setCategory(category.findCategoryById(id));
-        skillset.setSkills(skills);
-        
-        return skillset;
+        return skillsDao.getSkillsByCategoryId(id, acceptHeader, path);
 
    }
 
     @GET
     @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
     @Path("by/category/id/{id}/page/{page}")
-    public Skillset findSkillsByCategoryIdAndPage(@PathParam("id") int id,
-                                                  @PathParam("page") @DefaultValue("1") int page,
-                                                  @HeaderParam("Accept") String acceptHeader
-                                            ){
-        
-        currentPage = (page>=1)?page:1;
+    public MasterSkillset findSkillsByCategoryIdAndPage(@PathParam("id") int id,
+                                                        @PathParam("page") @DefaultValue("1") int page,
+                                                        @HeaderParam("Accept") String acceptHeader,
+                                                        @Context UriInfo uri){
 
-        from = (currentPage-1)*ROWS_PER_PAGE;
-        to = (from+ROWS_PER_PAGE);
-        
-        List<Skills> skills = skillsDao.findSkillsByCategoryId(id);
-        rowCount = skills.size();
-        
-        to = (to>rowCount)?rowCount:to;
-        
-        skills = skills.subList(from,to); 
-        Pagination pagination = new Pagination(currentPage,ROWS_PER_PAGE,id,rowCount,acceptHeader);
-        
-        skillset.setPagination(pagination);
-        skillset.setCategory(category.findCategoryById(id));
-        skillset.setSkills(skills);
-        
-        return skillset;
+        String path = categoryDao.getRealPath(uri.getAbsolutePath().toString());
+
+        return skillsDao.getSkillsByCategoryId(id, acceptHeader, path, page);
         
     }
     
@@ -93,8 +58,10 @@ public class SkillsetResource {
     @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
     @Path("by/id/{id}")
     public List<Skills> findSkillsById(@PathParam("id") int id,
-                                   @HeaderParam("Accept") String acceptHeader){
-
+                                       @HeaderParam("Accept") String acceptHeader,
+                                       @Context UriInfo uri){
+        
+        String path = uri.getAbsolutePath().toString();
         return skillsDao.findSkillsById(id);
 
     }
