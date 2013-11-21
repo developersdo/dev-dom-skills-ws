179967d1345a20b2163f1a4b1cbc6218a31b62a1
Carlos Vásquez Polanco <carlosslim@gmail.com>
2013-11-20 23:23:13 -0400

    Agregando nuevos mètodos para listar informaciòn de universidad y registro de los recursos para ser mostrados via HTML

--- src/main/java/org/devdom/skills/service/UniversityResource.java
+++ src/main/java/org/devdom/skills/service/UniversityResource.java
@@ -3,6 +3,7 @@ package org.devdom.skills.service;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -23,12 +24,25 @@ public class UniversityResource {
     
     @GET
     @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-    public MasterUniversity index(@HeaderParam("Accept") String acceptHeader,
-                                 @Context UriInfo uri){
+    public MasterUniversity findAllUniversity(@HeaderParam("Accept") String acceptHeader,
+                                              @Context UriInfo uri){
 
         String path = categoryDao.getRealPath(uri.getAbsolutePath().toString());
 
         return universityDao.getAllUniversities(acceptHeader, path);
+
+    }
+    
+    @GET
+    @Path("page/{page}")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public MasterUniversity findAllUniversityAndPage(@HeaderParam("Accept") String acceptHeader,
+                                                     @PathParam("page") int page,
+                                                     @Context UriInfo uri){
+
+        String path = categoryDao.getRealPath(uri.getAbsolutePath().toString());
+
+        return universityDao.getAllUniversities(acceptHeader, path, page);
 
     }
     
